a525569f35c4c81b2406618576277bd1719a42c0
Flychuban <kaloanas07@gmail.com>
2024-06-11 19:10:04 +0300

    add code for create_image and fix errors

--- quadtree_compression.cpp
+++ quadtree_compression.cpp
@@ -12,7 +12,7 @@ using namespace cv;
 // Calculating average color of a region
 Vec3b average_color(const Mat& image){
     // mean() function calculates the average of all the pixels in the image -> OpenCV
-    Scalar avg_color = mean(image); // Scalar - BGR channels
+    Scalar average_color = mean(image); // Scalar - BGR channels
     // Vec3b - 3-channel vector of 8-bit unsigned integers
     return Vec3b((uchar)average_color[0], (uchar)average_color[1], (uchar)average_color[2]); // 8-bit unsigned integer - BGR channels
 }
@@ -74,7 +74,7 @@ public:
     Rect bbox;
     int depth;
     bool is_leaf;
-    vector<Quadrant> children;
+    vector<Quadrant*> children;
     Vec3b color;
     double detail;
 
@@ -83,18 +83,18 @@ public:
     : bbox(bbox), depth(depth), is_leaf(false), children(4, nullptr)
     {
         Mat region = image(bbox);
-        detail = get_detail(region);
-        color = average_color(region);
+        this->detail = get_detail(region);
+        this->color = average_color(region);
     }
 
-    void split_region(const Mat&){
+    void split_region(const Mat& image){
         int middle_x = bbox.x + bbox.width / 2;
         int middle_y = bbox.y + bbox.height / 2;
 
-        children[0] = Quadrant(image, Rect(bbox.x, bbox.y, bbox.width / 2, bbox.height / 2), depth + 1); // Top-left
-        children[1] = Quadrant(image, Rect(middle_x, bbox.y, bbox.width / 2, bbox.height / 2), depth + 1); // Top-right
-        children[2] = Quadrant(image, Rect(bbox.x, middle_y, bbox.width / 2, bbox.height / 2), depth + 1); // Bottom-left
-        children[3] = Quadrant(image, Rect(middle_x, middle_y, bbox.width / 2, bbox.height / 2), depth + 1); // Bottom-right
+        this->children[0] = new Quadrant(image, Rect(bbox.x, bbox.y, bbox.width / 2, bbox.height / 2), depth + 1); // Top-left
+        this->children[1] = new Quadrant(image, Rect(middle_x, bbox.y, bbox.width / 2, bbox.height / 2), depth + 1); // Top-right
+        this->children[2] = new Quadrant(image, Rect(bbox.x, middle_y, bbox.width / 2, bbox.height / 2), depth + 1); // Bottom-left
+        this->children[3] = new Quadrant(image, Rect(middle_x, middle_y, bbox.width / 2, bbox.height / 2), depth + 1); // Bottom-right
     }
 };
 
@@ -103,25 +103,69 @@ public:
     Quadrant* root;
     int max_depth;
 
+    // Initialize with zero depth and the root quadrant
     QuadTree(const Mat& image){
-        max_depth = 0;
-        root = new Quadrant(image, Rect(0, 0, image.cols, image.rows), 0);
+        this->max_depth = 0;
+        this->root = new Quadrant(image, Rect(0, 0, image.cols, image.rows), 0);
     }
 
     // Recursively build the quadtree by splitting the regions
-    void build(Quadrant* quad, const Mat& iamge){
+    void build(Quadrant* quad, const Mat& image){
         if (quad->depth >= MAX_DEPTH || quad->detail < DETAIL_THRESHOLD){ // Stop splitting the region
-            if (quad->depth > max_depth){
-                max_depth = quad->depth;
+            if (quad->depth > this->max_depth){
+                this->max_depth = quad->depth;
             }
             quad->is_leaf = true;
             return; 
         }
         else{
             quad->split_region(image);
-            for (int i = 0; i < 4; i++){
-                build(&quad->children[i], image);
+            for(auto& child : quad->children){
+                build(child, image);
+            }
+        }
+    }
+
+    void draw_quadrants(Mat& image, Quadrant* quad, int depth, bool show_lines){
+        if (quad->is_leaf == depth || quad->is_leaf){
+            // reatangle() function draws a rectangle on the image -> OpenCV
+            rectangle(image, quad->bbox, Scalar(quad->color[0], quad->color[1], quad->color[2]), FILLED); // Fill the rectangle with the average color of the region
+            if (show_lines){
+                rectangle(image, quad->bbox, Scalar(0, 0, 0), 1); // Draw the lines of the rectangle
+            }            
+        }
+        else{
+            for(auto& child : quad->children){
+                if (!child)
+                {
+                    draw_quadrants(image, child, depth, show_lines);
+                }
+            }
+        }
+    }
+
+    Mat create_image(int depth, bool show_lines){
+        // CV_8UC3 - 8-bit unsigned integer matrix with 3 channels
+        Mat image = Mat::zeros(this->root->bbox.height, root->bbox.width, CV_8UC3); // Create a black imagec
+        draw_quadrants(image, this->root, depth, show_lines);
+        return image;
+    }
+
+    ~QuadTree(){
+        free_quadtree(this->root);
+    }
+
+private:
+    void free_quadtree(Quadrant* quad){
+        for(auto& child : quad->children){
+            if (!child){
+                free_quadtree(child);
+                delete &child;
             }
         }
     }
 };
+
+int main(){
+    cout << "QuadTree Image Compression" << endl;
+}
